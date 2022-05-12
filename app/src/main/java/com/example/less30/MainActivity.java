91a81964162649c4b1b18d788621b0a95df87288
BogdanBaz <69531999+BogdanBaz@users.noreply.github.com>
2022-05-12 20:48:00 +0200

    commit_overlay

--- app/src/main/java/com/example/less30/MainActivity.java
+++ app/src/main/java/com/example/less30/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.less30;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +34,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -30,7 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     final private int GRAVITY = 2;
 
     private TextView textView;
-    Button btnColor, btnGravity, btnEn, btnPl;
+    Button btnColor, btnGravity, btnEn, btnPl,overlayPowerBtn;
+    WindowManager windowManager;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -84,28 +102,196 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 && ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 permissions[1]) == PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show();
-        }
-        else
+        } else
             // Permission is not granted
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 Toast.makeText(this, "shouldShowRequestPermissionRationale  - TRUE", Toast.LENGTH_SHORT).show();
                 ActivityCompat.requestPermissions(this,
-                        permissions,1);
+                        permissions, 1);
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
             } else {
                 // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(this,
-                        permissions,1);
+                        permissions, 1);
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
+
+        if (Build.VERSION.SDK_INT >= 23 && !Settings.canDrawOverlays(this)) {
+            CharSequence text = "Please grant the access to the application.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.fromParts("package", getPackageName(), null)));
+        } else {
+            startPowerOverlay();
+        }
+
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private void startPowerOverlay() {
+        // Starts the button overlay.
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        overlayPowerBtn = new Button(this);
+        overlayPowerBtn.setText("OVERLAY");
+
+        int LAYOUT_FLAG;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // APPLICATION_OVERLAY FOR ANDROID 26+ AS THE PREVIOUS VERSION RAISES ERRORS
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            // FOR PREVIOUS VERSIONS USE TYPE_PHONE AS THE NEW VERSION IS NOT SUPPORTED
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = 0;
+        params.y = 100;
+        params.height = 110;
+        params.width = 110;
+
+        windowManager.addView(overlayPowerBtn, params);
+
+        overlayPowerBtn.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+            private long latestPressTime = 0;
+            private int CLICK_ACTION_THRESHOLD = 1;
+            private float startX;
+            private float startY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startX = event.getX();
+                        startY = event.getY();
+                        // Save current x/y
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        // Check for double clicks.
+                       /* if (latestPressTime == 0 || latestPressTime + 500 < System.currentTimeMillis()) {
+                            latestPressTime = System.currentTimeMillis();
+                        } else {
+                            Toast.makeText(MainActivity.this, "2x click", Toast.LENGTH_SHORT).show();
+                            // Doubleclicked. Do any action you'd like
+                        }*/
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        float endX = event.getX();
+                        float endY = event.getY();
+                        if (isAClick(startX, endX, startY, endY)) {
+                            takeScreenshot();
+                            Toast.makeText(MainActivity.this, "CLICK ??? ", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(overlayPowerBtn, params);
+                        return true;
+                }
+                return false;
+            }
+
+            private boolean isAClick(float startX, float endX, float startY, float endY) {
+                float differenceX = Math.abs(startX - endX);
+                float differenceY = Math.abs(startY - endY);
+                return !(differenceX > CLICK_ACTION_THRESHOLD/* =5 */ || differenceY > CLICK_ACTION_THRESHOLD);
+            }
+
+        });
+        overlayPowerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "CLICK", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void takeScreenshot() {
+        // create bitmap screen capture
+        View v1 = getWindow().getDecorView().getRootView();
+        v1.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+        v1.setDrawingCacheEnabled(false);
+        try {
+            store(bitmap, "newFUCKING_SCREEN");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (overlayPowerBtn != null)
+            windowManager.removeView(overlayPowerBtn);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (overlayPowerBtn != null)
+            windowManager.removeView(overlayPowerBtn);
+    }
+
+    private void store(Bitmap bitmap, String s)throws IOException {
+        boolean saved;
+        OutputStream fos;
+        File image = null;
+        Uri imageUri = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+            ContentResolver resolver = MainActivity.this.getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, s);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/" );
+             imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+            fos = resolver.openOutputStream(imageUri);
+        } else {
+            Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+            String imagesDir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).toString() + File.separator ;
+
+            File file = new File(imagesDir);
+
+            if (!file.exists()) {
+                file.mkdir();
+            }
+
+             image = new File(imagesDir, s + ".png");
+            fos = new FileOutputStream(image);
+        }
+
+        saved = bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        fos.flush();
+        fos.close();
+        openScreenshot(imageUri);
+       // openScreenshot(image);
+    }
+    
+    private void openScreenshot(Uri imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+       // Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(imageFile, "image/*");
+        startActivity(intent);
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
